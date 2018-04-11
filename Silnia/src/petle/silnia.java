1aa45c6793d01f3bb3d77ad9ad80e91a245e934f
micbarPOL <37364623+micbarPOL@users.noreply.github.com>
2018-04-11 15:41:46 +0200

    Add files via upload
    silnia

---
+++ Silnia/src/petle/silnia.java
@@ -0,0 +1,22 @@
+package petle;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class silnia {
+
+	public static void main(String[] args) {
+		BigInteger wynik = new BigInteger("1");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Podaj liczbe...");
+		int n = sc.nextInt();
+		System.out.println("Liczę silnię dla " + n);
+		
+		for (int i = 1; i < n + 1; i++) {
+			wynik = wynik.multiply(BigInteger.valueOf(i));
+		}
+		System.out.println(n + "! = " + wynik);
+		sc.close();
+	}
+
+}
