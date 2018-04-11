4765c6b184332bc0e1b0f93d181231077504b1dd
micbarPOL <37364623+micbarPOL@users.noreply.github.com>
2018-04-11 17:03:24 +0200

    Update ZbieranieDanych.java

--- src/kolekcje/ZbieranieDanych.java
+++ src/kolekcje/ZbieranieDanych.java
@@ -1,28 +1,58 @@
 package kolekcje;
 
-
+import java.text.Collator;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 import javax.swing.JOptionPane;
-public class ZbieranieDanych {
 
-	public static void main(String[] args) {
-		String napis = "";
-		List<String> listaImion = new ArrayList<>();
-		
-		
-		while (napis != null) {
-			napis = JOptionPane.showInputDialog("Podaj imie");
-			listaImion.add(napis);
-		} 
-		
-		listaImion.remove(null);
-		System.out.println("Rozmiar listy: " + listaImion.size());
-		System.out.println(listaImion);
-		Collections.sort(listaImion);
-		System.out.println(listaImion);
-	}
+public class ZbieranieDanych {
+    // w pętli pytać użytkownika za pomocą JOptionPane o kolejne napisy
+    // te napisy dodawać do listy
+    // przerwać kiedy użytkownik wciśnie Cancel        
+
+    public static void main(String[] args) {
+        List<String> lista = new ArrayList<>();
+        
+        while(true) {
+            String napis = JOptionPane.showInputDialog("Podaj kolejny napis");
+            if(napis == null) {
+                break;
+            }
+            lista.add(napis);
+        }
+        
+        System.out.println("Rozmiar listy: " + lista.size());
+        System.out.println(lista);
+
+        // sortowanie w sposób domyślny (wg kodów Unicode), zapis dostępny od "bardzo dawna"
+        Collections.sort(lista);
+        System.out.println(lista);
+        
+        // sortowanie wg alfabetu języka "ludzkiego"
+        // wg bieżących ustawień językowych komputera
+        Collections.sort(lista, Collator.getInstance());
+        System.out.println(lista);
+
+        // wg podanych "lokali"
+        Collections.sort(lista, Collator.getInstance(Locale.FRANCE));
+        System.out.println(lista);
+
+        Collections.sort(lista, Collator.getInstance(new Locale("pl", "PL")));
+        System.out.println(lista);
+
+        // zapis dostepny od Javy 8:
+        lista.sort(Collator.getInstance());
+        System.out.println(lista);
+        
+	    //sortowanie lambdami. System porowna dlugosci wyrazow i posortuje po nich
+        lista.sort((x,y) -> x.length() - y.length());
+        System.out.println(lista);
+    }
 
 }
+
+
+
