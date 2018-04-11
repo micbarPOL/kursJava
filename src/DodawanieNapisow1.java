5638e30442cc794beff315a6962bfec3d0abb865
micbar <michal.bardadyn@gmail.com>
2018-04-11 15:30:38 +0200

    pierwsza klasa do tworzenia stringow

--- src/DodawanieNapisow1.java
+++ src/DodawanieNapisow1.java
@@ -2,8 +2,16 @@
 public class DodawanieNapisow1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(kolejneLiczby(24));
+	}
 
+	static String kolejneLiczby(int n) {
+		String wartosc = "";
+		for (int i = 0; i<=n; i++) {
+			wartosc += i;
+			wartosc += " ";
+		}
+		return wartosc;
 	}
 
 }
