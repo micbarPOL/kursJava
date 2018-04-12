ae133f29c8f0ca276973f67c680a3feca5461f2d
micbar <michal.bardadyn@gmail.com>
2018-04-12 15:29:18 +0200

    pierwszy udany zapis

--- P10X Pracownicy/src/pliki/Zapis1.java
+++ P10X Pracownicy/src/pliki/Zapis1.java
@@ -2,6 +2,7 @@ package pliki;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.time.ZonedDateTime;
 
 public class Zapis1 {
 
@@ -10,8 +11,11 @@ public class Zapis1 {
 		try {
 			PrintWriter out = new PrintWriter("C:/Users/kurs/Desktop/plik1.txt");
 			
-			//samo println nic nie da. Pojawi sie plik bo go tworzymy z obiektem ale nic sie nie zapisze
+			//samo println nic nie da. Pojawi sie plik bo go tworzymy z obiektem ale nic sie nie zapisze bo plik nie jest zamkniety
+			//trzeba dodac np out.close()
 			out.println("XXX");
+			out.println(ZonedDateTime.now());
+			out.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
