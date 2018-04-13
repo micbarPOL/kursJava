bdbc79b14541f60f7ee3ac26c2e6b94e3b87369f
micbar <michal.bardadyn@gmail.com>
2018-04-13 09:54:26 +0200

    odczytywanie i wypisywanie na konsole

--- P10X Pracownicy/src/Pracownicy/ObslugaCSV.java
+++ P10X Pracownicy/src/Pracownicy/ObslugaCSV.java
@@ -1,7 +1,10 @@
 package Pracownicy;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ObslugaCSV {
 
@@ -9,6 +12,26 @@ public class ObslugaCSV {
 
 		// tworze liste pracownikow do ktorej bede dorzucal pracownikow
 		List<Pracownik> listaPracownikow = new ArrayList<>();
+
+		// trzeba utworzyc obiekt klasy file bo Scanner z niego korzysta.
+		// daloby sie dac parametr string do Scnaerra ale wtedy by odczytał tego stringa
+		// a nie zaden plik
+		File plik = new File(sciezka);
+
+		// jesli uzywam takiego try(deklaracja scannera) to nie musze na koncu pisac
+		// sc.colse(). Jesli cos by bylo zle to scanner sam sie zamknie
+		try (Scanner sc = new Scanner(plik)) {
+			while (sc.hasNextLine()) {
+				String linia = sc.nextLine();
+				
+				//split zwraca w wyniku tablice stringow (mozna to sprawdzic najezdzajac mysza na split)
+				String[] chunk = linia.split(";");
+				System.out.println(chunk[3]);
+
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 
 		return listaPracownikow;
 	}
