547ca43bcf4ac448a941ed9084b9d43c0b96ec3a
micbar <michal.bardadyn@gmail.com>
2018-05-11 10:42:12 +0200

    kalkulator

--- Serwlety/src/Serwlety/InfoBean.java
+++ Serwlety/src/Serwlety/InfoBean.java
@@ -1,5 +1,7 @@
 package Serwlety;
 
+import java.time.LocalTime;
+
 public class InfoBean {
 	// przykład klasy JavaBean
 	// do obiektow takiej klasy mozna odwolywac sie z poziomu JSP a w zasadzie z
@@ -7,4 +9,21 @@ public class InfoBean {
 
 	// Klasa ta musi posiadac konstruktor bezargumentowy
 	// klasa musi posiadac gettery i settery ktore maja dostep do propertiesow
+
+	private String tekst = "Poczatkowy tekst";
+
+	public String getTekst() {
+		return tekst;
+	}
+
+	public void setTekst(String tekst) {
+		this.tekst = tekst;
+	}
+
+	// mozna tez stworzc gettera ktory tak naparwde nie jest samym odczytem
+	// zmiennej. Ma w sobie kod
+	public LocalTime getCurrentTime() {
+		return LocalTime.now();
+	}
+
 }
